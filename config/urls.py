84501487b96d53b4442a1e5952e14c462ebc207a
Abdulaziz <abdulazizilla200709@gmail.com>
2022-08-16 19:30:37 +0500

    added user application

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('', include('user.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
